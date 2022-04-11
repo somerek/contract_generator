4b7c21cb209d73ece17be2d1d9e7a5e302d9466a
somerek <somerek@yandex.ru>
2022-04-11 09:49:31 +0300

    some tuning

--- run.sh
+++ run.sh
@@ -8,7 +8,3 @@ docker-compose build --build-arg backend_host_port=$backend_host_port \
     --build-arg POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
     --build-arg POSTGRES_DB=$POSTGRES_DB
 docker-compose up -d
-
-echo docker exec -it contract_db sh
-echo "psql -U $POSTGRES_USER -d $POSTGRES_DB"
-echo "\\dt;"
